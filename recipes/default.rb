41b031845dac9d24cc963e53e2427c6504d8a46e
Hedgehog <hedgehogshiatus@gmail.com>
2011-03-18 11:44:31 +1100

    host cfg for chef server search as dns

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,16 @@
+# Find all nodes, sorting by Chef ID so their
+# order doesn't change between runs.
+hosts = search(:node, "*:*", "X_CHEF_id_CHEF_X asc")
+chef_server = search(:node, 'run_list:recipe\[chef\:\:server\]')
+
 template "/etc/hosts" do
   source "hosts.erb"
   owner "root"
   group "root"
   mode 0644
+  variables(
+    :hosts => hosts,
+    :fqdn => node[:fqdn],
+    :hostname => node[:hostname]
+  )
 end
